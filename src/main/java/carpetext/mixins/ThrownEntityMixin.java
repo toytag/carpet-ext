1decd4255229256b705ec39c6853db122fb27da7
Zhenzhong Tang <tangzhzh@shanghaitech.edu.cn>
2021-02-06 15:01:03 +0800

    1.0.3

--- src/main/java/carpetext/mixins/ThrownEntityMixin.java
+++ src/main/java/carpetext/mixins/ThrownEntityMixin.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.util.Comparator;
 
 // MINECRAFT
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.projectile.thrown.EnderPearlEntity;
 import net.minecraft.entity.projectile.thrown.ThrownEntity;
@@ -22,13 +23,14 @@ import net.minecraft.world.World;
 
 // MIXIN
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
-@Mixin(EnderPearlEntity.class)
-public abstract class EnderPearlEntityMixin extends ThrownEntity {
+@Mixin(ThrownEntity.class)
+public abstract class ThrownEntityMixin extends Entity{
     private static final ChunkTicketType<ChunkPos> ENDER_PEARL_TICKET = 
         ChunkTicketType.create("ender_pearl", Comparator.comparingLong(ChunkPos::toLong), 2);
 
@@ -36,13 +38,18 @@ public abstract class EnderPearlEntityMixin extends ThrownEntity {
     private Vec3d realPos = null;
     private Vec3d realVelocity = null;
 
-    protected EnderPearlEntityMixin(EntityType<? extends ThrownEntity> entityType, World world) {
+    protected ThrownEntityMixin(EntityType<? extends Entity> entityType, World world) {
         super(entityType, world);
     }
 
-    @Inject(method = "tick", at = @At(value = "HEAD"))
+    @Shadow
+    protected abstract float getGravity();
+
+    @Inject(method = "tick()V", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/entity/projectile/thrown/ThrownEntity;getVelocity()Lnet/minecraft/util/math/Vec3d;",
+            ordinal = 0))
     private void prepareFutureChunks(CallbackInfo ci) {
-        if (CarpetExtSettings.enderPearlSkippyChunkLoading) {
+        if (CarpetExtSettings.enderPearlSkippyChunkLoading && ((Object) this) instanceof EnderPearlEntity) {
             this.skippyChunkLoading();
         }
     }
